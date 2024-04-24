ab7c96f0f82044180ca159d891fd4edae3568295
Esteban <88396858+luvxpluto@users.noreply.github.com>
2024-04-24 02:58:32 -0600

    base64

--- proyectoDisenno/src/main/java/com/web/proyectoDisenno/thirdparty/Speech.java
+++ proyectoDisenno/src/main/java/com/web/proyectoDisenno/thirdparty/Speech.java
@@ -1,12 +1,15 @@
 package com.web.proyectoDisenno.thirdparty;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
-
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Base64;
 
 public class Speech {
 
@@ -16,11 +19,18 @@ public class Speech {
 
   // Constructor privado para evitar instanciación externa
   private Speech() throws IOException {
-    // Asegurarse que las credenciales están configuradas
-    System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+    // Decodificar las credenciales desde la variable de entorno
+    String base64Credentials = System.getenv("GOOGLE_CREDENTIALS_BASE64");
+    byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+    try (InputStream credentialsStream = new ByteArrayInputStream(decodedBytes)) {
+      GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
+      TextToSpeechSettings settings = TextToSpeechSettings.newBuilder()
+              .setCredentialsProvider(() -> credentials)
+              .build();
 
-    // Inicializar el cliente de Google Text-to-Speech
-    textToSpeechClient = TextToSpeechClient.create();
+      // Inicializar el cliente de Google Text-to-Speech con las credenciales configuradas
+      textToSpeechClient = TextToSpeechClient.create(settings);
+    }
   }
 
   // Método público estático para obtener la instancia
